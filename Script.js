2cd577e84a506050838f6329677265531ab74155
louiebergendal <louie.bergendal@gmail.com>
2020-11-04 12:57:36 +0100

    Update Script.js
    konsollar en öl.

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
-console.log("test")
+
+function fetchBeer(number){
+
+    fetch("https://api.punkapi.com/v2/beers/" + number)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+}
+
+fetchBeer(1);
+fetchBeer(2);
+fetchBeer(3);
+

46b4ed7e9310f2d300c2c2af55f561f8e4053a5b
FelixGrahn <71120484+FelixGrahn@users.noreply.github.com>
2020-11-04 13:07:52 +0100

    Fixed 0 fetch return

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ console.log("test")
 RandomNumber();
 
 async function RandomNumber() {
-    let random = Math.floor(Math.random() * 10);
+    let random = Math.floor(Math.random() * 10) + 1;
     console.log(random);
     
     fetchBeer(random);

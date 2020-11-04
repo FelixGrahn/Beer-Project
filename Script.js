6720573caae468a13e44f30c14b4d0fadf888508
louiebergendal <louie.bergendal@gmail.com>
2020-11-04 13:00:39 +0100

    Revert "Update Script.js"
    This reverts commit 2cd577e84a506050838f6329677265531ab74155.

--- Script.js
+++ Script.js
@@ -1,13 +1,1 @@
-
-function fetchBeer(number){
-
-    fetch("https://api.punkapi.com/v2/beers/" + number)
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-}
-
-fetchBeer(1);
-fetchBeer(2);
-fetchBeer(3);
-
+console.log("test")

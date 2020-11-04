77422279f8c8a4c0fe0a74ea1441870fa6f2b9bb
louiebergendal <louie.bergendal@gmail.com>
2020-11-04 13:04:59 +0100

    conected random with fetch

--- Script.js
+++ Script.js
@@ -6,4 +6,13 @@ async function RandomNumber() {
     let random = Math.floor(Math.random() * 10);
     console.log(random);
     
+    fetchBeer(random);
 }
+
+async function fetchBeer(number){
+
+    fetch("https://api.punkapi.com/v2/beers/" + number)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+

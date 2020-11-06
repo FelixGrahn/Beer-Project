ab8d9c342af2ba36567f8e9254e82aeee1e58d4d
FelixGrahn <71120484+FelixGrahn@users.noreply.github.com>
2020-11-06 08:38:34 +0100

    Apllied Louies fix to code

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 console.log("test")
 
-//RandomNumber();
+RandomNumber();
 
 async function RandomNumber() {
     let random = Math.floor(Math.random() * 10) + 1;
@@ -13,16 +13,80 @@ async function fetchBeer(number){
 
     fetch("https://api.punkapi.com/v2/beers/" + number)
     .then(response => response.json())
-    .then(data => {
-        //console.log(data)
-        writeBeerInfo(data)
+    .then(url => {
+        console.log(url)
+        console.log(url[0].description)
+        console.log(url[0].image_url)
+        console.log(url[0].name)
+        writeBeerInfo(url)
+        
     } );
 
     
 }
 
-async function writeBeerInfo(data) {
+async function writeBeerInfo(url) {
+    let writebox = document.querySelector(".grid-item-2");
+    let writeboxinfo = document.createElement("section");
+    /* writeboxinfo.innerHTML = (
+        url.image_url
+    ) */
+    writeboxinfo.innerText = (
+        /* fetchimage(url) + */
+        url[0].name +
+        url[0].description
+    )
+    writebox.appendChild(writeboxinfo);
     console.log("connection succses")
-    console.log(data)
+    /* console.log(url) */
     
 }
+
+async function fetchimage(url) {
+    let imageurl = url.image_url;
+    fetch(imageurl)
+    .then(response => response.json())
+    .then(image => {
+        return(image)
+    } );
+}
+
+async function writeingridients(url) {
+
+    for (let index = 0; index < url.ingredients.length; index++) {
+        
+        
+    }
+    
+}
+
+
+ 
+/* const grabRandomBeer = () => {
+    
+    fetch("https://api.punkapi.com/v2/beers/random")
+      .then(response => response.json())
+      .then(beers => {  
+        const beer = beers[0];
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = beer.name;
+        const descriptionElement = document.createElement("p");
+        descriptionElement.textContent = beer.description;
+        const abvElement = document.createElement("p");
+        abvElement.textContent = `Alcohol by volume: ${beer.abv}%`;
+        const firstBrewed = document.createElement("span");
+        firstBrewed.textContent = `First brewed on: ${beer.first_brewed}`;
+        const beerElement = document.getElementById("beer");
+        beerElement.innerHTML = "";
+        beerElement.appendChild(nameElement);
+        beerElement.appendChild(descriptionElement);
+        beerElement.appendChild(abvElement);
+        beerElement.appendChild(firstBrewed);
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  };
+  
+  // Grab a new beer when clicking the button
+  document.getElementById("grabButton").addEventListener("click", grabRandomBeer); */
